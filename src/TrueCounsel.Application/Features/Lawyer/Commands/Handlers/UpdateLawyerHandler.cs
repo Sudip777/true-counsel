bb8c6d575e5c83988f08ad61a505ced9cb137d13
sudip <parajulisudip06@gmail.com>
2025-12-31 21:50:02 +0545

    feat(lawyer): add fluent validation on lawyers

--- src/TrueCounsel.Application/Features/Lawyer/Commands/Handlers/UpdateLawyerHandler.cs
+++ src/TrueCounsel.Application/Features/Lawyer/Commands/Handlers/UpdateLawyerHandler.cs
@@ -12,17 +12,25 @@ namespace TrueCounsel.Application.Features.Lawyer.Commands.Handlers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
+        private readonly FluentValidation.IValidator<UpdateLawyerCommand> _validator;
 
-        public UpdateLawyerHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        public UpdateLawyerHandler(IUnitOfWork unitOfWork, IMapper mapper, FluentValidation.IValidator<UpdateLawyerCommand> validator)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _validator = validator;
         }
 
         /// <summary> updates exsting lawer deetails </summary>
         public async Task<LawyerDto> HandleAsync(UpdateLawyerCommand command, CancellationToken cancellationToken = default)
         {
              ArgumentNullException.ThrowIfNull(command);
+
+             var validationResult = await _validator.ValidateAsync(command, cancellationToken);
+             if (!validationResult.IsValid)
+             {
+                 throw new TrueCounsel.Application.Common.Exceptions.AppValidationException(validationResult.Errors);
+             }
 
              var lawyer = await _unitOfWork.LawyerRepository.GetByIdAsync(command.Id);
              if (lawyer == null) throw new Exception($"Lawyer with id {command.Id} not found");
